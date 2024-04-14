f41ce4f41a55929ad1f1bd0981e4dd8afb1f7b3f
GerardoXL <leon.capo2000@gmail.com>
2024-04-13 23:57:11 -0300

    correccion del ejercicio4 y se agrega el ejercicio5

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio5/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		///TODO Auto-generated method stub
 		
 		Scanner entrada = new Scanner(System.in);
 		
