e904edc4c207bf99e432d5b0672016af4ca43860
GerardoXL <leon.capo2000@gmail.com>
2024-04-15 21:36:14 -0300

    avance del ejercicio6 para ingresar los datos de la segunda persona

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio6/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio6/main/Main.java
@@ -1,6 +1,8 @@
 package ar.edu.unju.fi.ejercicio6.main;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 import ar.edu.unju.fi.ejercicio6.model.Persona;
 
@@ -9,18 +11,77 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+        Scanner scanner = new Scanner(System.in);
+		
+		
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		
 		//crea una instacia persona mediante un constructor vacio
 		
 		Persona Persona = new Persona();
 		
-		// se establece valores de los atributos
+		//se ingresa los datos de la primera persona 
 		
-		Persona.setDNI(41653761);
-		Persona.setNombre(" Mario ");
-		Persona.setFecha_de_nacimiento(LocalDate.of(1999, 01, 21));
-		Persona.setProvincia(" Jujuy ");
+		System.out.println("Ingrese los datos de la primera persona");
 		
-		Persona.MostrarLosDatos();
+		System.out.println("DNI: ");
+		
+		Persona.setDNI(scanner.nextInt());
+		
+		scanner.nextLine();
+
+		System.out.println("Nombre: ");
+		
+		Persona.setNombre(scanner.nextLine());
+		
+		System.out.println("Fecha de nacimiento: ");
+		
+		Persona.setFecha_de_nacimiento(LocalDate.parse(scanner.nextLine(),formato));
+		
+		System.out.println("Provincia: ");
+		
+		Persona.setProvincia(scanner.nextLine());
+		
+        Persona.MostrarLosDatos();
+        
+        
+        
+        
+        System.out.println("ingrese los datos de la segunda persona");
+        
+        System.out.println("DNI: ");
+        
+        int DNI = scanner.nextInt();
+        
+        scanner.nextLine();
+        
+        System.out.println("Nombre: ");
+        
+        String nombre = scanner.nextLine();
+        
+        System.out.println("Fecha de nacimiento: ");
+        
+        LocalDate fecha_de_nacimiento = LocalDate.parse(scanner.nextLine(), formato);
+        
+        System.out.println("Provincia: ");
+        
+        String provincia = scanner.nextLine();
+        
+        Persona = Persona = new Persona(DNI,nombre,fecha_de_nacimiento,provincia);
+        
+        Persona Persona2 = Persona;
+        
+        Persona2.MostrarLosDatos();
+        
+        
+        
+        
+        
+        
+		
+		
+		
 		
 		
 		
