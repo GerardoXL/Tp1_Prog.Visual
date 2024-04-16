db45248449a3bcdfc033187e039ce7dd418449e8
GerardoXL <leon.capo2000@gmail.com>
2024-04-16 00:37:04 -0300

    avance del ejercicio7

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio7/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio7/main/Main.java
@@ -1,9 +1,49 @@
 package ar.edu.unju.fi.ejercicio7.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio7.model.Empleado;
+
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		System.out.println("ingrese el nombre del empleado: ");
+		
+		String nombre = entrada.nextLine();
+		
+		System.out.println("ingrese el legajo del empleado: ");
+		
+		int legajo = entrada.nextInt();
+		
+		System.out.println("ingrese el salario del empleado: ");
+		
+		double salario = entrada.nextDouble();
+		
+		//se crea el objeto empleado
+		Empleado empleado = new Empleado (nombre, legajo, salario);
+		
+		//se muestra el salario 
+		
+		empleado.mostrarlosDatos();
+		
+		//dar el aumento al salario
+		
+		empleado.DarAumento();
+		
+		//se muestra los datos del empleado luego del aumento 
+		
+		empleado.mostrarlosDatos();
+		
+		
+		
+		
 
 	}
 
--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio7/model/Empleado.java
@@ -10,12 +10,12 @@ public class Empleado {
 	private int legajo;
 	private double salario;
 	
-	
+	//constructor parametrizado
 	public Empleado(String nombre, int legajo, double salario) {
 		
 		this.nombre = nombre;
 		this.legajo = legajo;
-		this.salario = salario;
+		this.salario = salario < SALARIO_MINIMO ? SALARIO_MINIMO : salario;
 		
 		if (salario>=SALARIO_MINIMO) {
 			
@@ -28,10 +28,59 @@ public class Empleado {
 			
 		}
 		
-		
 	}
 	
 	
+public void mostrarlosDatos() {
+		
+		
+		System.out.println("Nombre del empleado: " + nombre);
+		
+		System.out.println("Legajo: " + legajo);
+		
+		System.out.println("Salario: " + salario+"$");
+		
+	}
+
+		
+		
+		//metodo para el aumento del salario 
 	
+	   public void DarAumento() {
+		   
+		   salario += AUMENTO_SALARIO;
+	   }
+	   
+	   //getters y setters
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	
+	
+	
+	   
+	   
 
 }
