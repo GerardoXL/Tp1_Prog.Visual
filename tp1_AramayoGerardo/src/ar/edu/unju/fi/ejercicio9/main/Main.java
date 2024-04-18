e4a083e70bc75da0f394a72121979001d1352084
GerardoXL <leon.capo2000@gmail.com>
2024-04-17 21:21:11 -0300

    avance del ejercicio9

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio9/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio9/main/Main.java
@@ -1,10 +1,63 @@
 package ar.edu.unju.fi.ejercicio9.main;
 
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio9.model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//objeto de la clase scanner para leer la entrada del usuario
+		
+		Scanner  entrada = new Scanner (System.in);
+		
+		//se crea un array para almacenar los productos
+		
+		Producto[] productos = new Producto[3];
+		
+		
+		for(int i=0; i<productos.length; i++){
+		
+		System.out.println("ingrese los datos del producto: " + (i + 1)+" : ");
+		
+		
+		productos[i] = new Producto();
+		
+		System.out.println("Nombre: ");
+		productos[i].setNombre(entrada.nextLine());
+		
+		
+		System.out.println("Codigo: ");
+		
+		productos[i].setCodigo(entrada.nextLine());
+		
+		System.out.println("Precio: ");
+		
+		productos[i].setPrecio(Double.parseDouble(entrada.nextLine()));
+		
+		System.out.println("Descuento: ");
+		
+		productos[i].setDescuento(Integer.parseInt(entrada.nextLine()));
+		
+		
+		}
+		
+		//se muestra los datos ingresados de cada producto y el resultado del descuento
+		
+		for(int i = 0; i < productos.length; i++){
+			
+			System.out.println("Datos del producto: " + (i+1) + ":");
+			
+			System.out.println("Nombre: " + productos[i].getNombre());
+			System.out.println("Codigo: " + productos[i].getCodigo());
+			System.out.println("Precio: " + productos[i].getPrecio()+"$");
+			System.out.println("Descuento: " + productos[i].CalculoDescuento()+"$");
+			
+			
+			
+		}
+		
 	}
-
 }
--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio9/model/Producto.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio9/model/Producto.java
@@ -1,5 +1,66 @@
 package ar.edu.unju.fi.ejercicio9.model;
 
 public class Producto {
+	
+	private String nombre;
+	private String codigo;
+	private double precio;
+	private double descuento;
+	
+	//constructor vacio o por defecto
+	
+	public Producto() {
+		
+		
+		
+	}
+	
+	//accesores getters and setters
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public double getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(double descuento) {
+		this.descuento = descuento;
+	}
+	
+	//metodo para calcular descuento
+	
+	public double CalculoDescuento() {
+		
+		return precio - (precio*descuento/100.0);
+		
+	}
+	
+	
+	
+	
+	
+	
 
 }
