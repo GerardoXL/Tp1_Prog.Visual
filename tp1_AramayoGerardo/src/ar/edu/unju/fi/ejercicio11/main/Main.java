072b980641540c52b942801b33df1abd7952ce1e
GerardoXL <leon.capo2000@gmail.com>
2024-04-20 00:29:03 -0300

    avance del ejercicio11

---
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio11/main/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio11.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		int j;
+		
+		for(i = 2, j = 40; j >0; i++,j --) {
+			
+			int resultado = i*j ;
+			
+			System.out.print( resultado + " " );
+			
+		}
+		
+
+	}
+
+}
