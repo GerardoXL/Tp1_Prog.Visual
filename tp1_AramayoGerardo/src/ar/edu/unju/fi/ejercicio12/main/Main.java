e5b5801f1595826312e1863e1033a1d3208465af
GerardoXL <leon.capo2000@gmail.com>
2024-04-20 18:46:05 -0300

    avances del ejercicio12

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio12/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio12/main/Main.java
@@ -1,12 +1,53 @@
 package ar.edu.unju.fi.ejercicio12.main;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import ar.edu.unju.fi.ejercicio12.model.Persona;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+        Scanner entrada = new Scanner(System.in);
 
+			
+		System.out.println("ingrese su nombre: ");
+		
+		String nombre = entrada.nextLine();
+		
+		System.out.println("ingrese su fecha de nacimiento: ");
+		
+		String fecha_de_nacimiento = entrada.nextLine();
+		
+		SimpleDateFormat fechaformato = new SimpleDateFormat("dd-MM-yyyy");
+		
+        Date fechaNacimientoDate = null;
+        
+        try {
+        	
+        	fechaNacimientoDate = fechaformato.parse(fecha_de_nacimiento);
+        	
+        } catch (Exception e) {
+        	
+        	System.out.println("formato de fecha incorrecto");
+        	System.exit(0);
+        }
+        
+        Calendar fechaNacimiento = Calendar.getInstance();
+        fechaNacimiento.setTime(fechaNacimientoDate);
+        
+        
+        Persona persona = new Persona (nombre, fechaNacimiento);
+        
+        
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Edad: " + persona.CalculoAñosPersona());
+        
 	}
 
+	
+	
+	
 }
--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio12/model/Persona.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio12/model/Persona.java
@@ -1,5 +1,75 @@
 package ar.edu.unju.fi.ejercicio12.model;
 
-public class Persona {
+import java.util.Calendar;
 
+public class Persona {
+	
+	private String nombre;
+	private Calendar fecha_de_nacimiento;
+	
+	
+	public Persona (String nombre, Calendar fecha_de_nacimiento) {
+		
+		this.nombre = nombre;
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+		
+	}
+	
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+
+
+	public Calendar getFecha_de_nacimiento() {
+		return fecha_de_nacimiento;
+	}
+
+
+
+
+
+
+	public void setFecha_de_nacimiento(Calendar fecha_de_nacimiento) {
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+	}
+
+
+
+
+	//metodos para determinar cuantos años tiene la persona
+	
+    public int CalculoAñosPersona(){
+		
+		Calendar fechaActual = Calendar.getInstance();
+		
+		int edad = fechaActual.get(Calendar.YEAR) - fecha_de_nacimiento.get(Calendar.YEAR);
+		
+		if(fechaActual.get(Calendar.DAY_OF_MONTH) > fecha_de_nacimiento.get(Calendar.DAY_OF_MONTH)
+				|| fecha_de_nacimiento.get(Calendar.MONTH) == fechaActual.get(Calendar.MONTH)
+				&& fecha_de_nacimiento.get(Calendar.DATE) > fechaActual.get(Calendar.DATE)) {
+		
+		edad --;
+		
+		}
+	   return edad;
+	
+	}
+    
 }
