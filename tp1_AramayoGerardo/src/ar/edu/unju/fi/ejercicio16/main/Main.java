8f1844484485d6c0f4fec4ed94ee0d63ea031867
GerardoXL <leon.capo2000@gmail.com>
2024-04-21 00:18:09 -0300

    avance del ejercicio16

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio16/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio16/main/Main.java
@@ -1,11 +1,23 @@
 package ar.edu.unju.fi.ejercicio16.main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner entrada = new Scanner(System.in);
+		String[] nombres = new String[5];
 		
+		
+		for(int i = 0; i < nombres.length; i++) {
+			
+			System.out.println("ingrese el nombre de una persona: " + (i+1) + ":");
+			
+			nombres[i] = entrada.nextLine();
+			
+		}
 
 	}
 
