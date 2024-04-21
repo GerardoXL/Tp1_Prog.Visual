a7791b3282bdc3481180322f6d33ecaf1cc5e969
GerardoXL <leon.capo2000@gmail.com>
2024-04-21 01:02:43 -0300

    avance del ejercicio16 (eliminacion y desplazamiento de los indices)

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio16/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio16/main/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		
 		for(int i = 0; i < nombres.length; i++) {
 			
-			System.out.println("ingrese el nombre de una persona: " + (i+1) + ":");
+			System.out.println("ingrese el nombre de una persona: " + (i) + ":");
 			
 			nombres[i] = entrada.nextLine();
 			
@@ -27,7 +27,7 @@ public class Main {
 		
 		while(indice < nombres.length) {
 			
-			System.out.println(nombres[indice]);
+			System.out.println("indice " + indice + " Nombre: " + nombres[indice]);
 			
 			indice++;
 		}
@@ -37,6 +37,44 @@ public class Main {
 		System.out.println("el tamaño del array es: " + nombres.length);
 		
 		
+		//para eliminar un elemento del array
+		
+		
+		byte EliminarIndice;
+		
+		do {
+			
+			System.out.println("ingrese el indice que desea eliminar: ");
+			
+			EliminarIndice = entrada.nextByte();
+			
+		} while (EliminarIndice < 0 || EliminarIndice >= nombres.length);
+		
+		
+		// para realizar un desplazamiento de elementos
+		
+		 for(int i = EliminarIndice; i < nombres.length - 1; i++) {
+			 
+			 nombres[i] = nombres[i+1];
+		 }
+		 
+		 
+		 //para asignar un espacio en blanco al ultimo elemento
+		 
+		 nombres[nombres.length - 1] = "";
+		 
+		 
+		 //para mostrar los nombres despues de eliminar el indice
+		 
+		 System.out.println("los nombres despues de eliminar el indice son: ");
+		 
+		 indice = 0;
+		 
+		 while(indice < nombres.length) {
+			 
+			 System.out.println(nombres[indice]);
+			 indice++;
+		 }
 		
 		
 		
