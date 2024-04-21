9101781ae654014d81c4a110f112a906850d757b
GerardoXL <leon.capo2000@gmail.com>
2024-04-20 23:07:49 -0300

    avance del ejericio14

--- tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio14/main/Main.java
+++ tp1_AramayoGerardo/src/ar/edu/unju/fi/ejercicio14/main/Main.java
@@ -25,6 +25,43 @@ public class Main {
 		
 		System.out.println("ingresaste el numero: " + numeros);
 		
+		
+		
+		//se crea un array de enteros de un tamaño ingresado por el usuario
+		
+		int[] array = new int[numeros];
+		
+		//acumulador para la suma de los numeros ingresados 
+		
+		int SumaDeArray = 0;
+		
+		//se llena el array con los numeros ingresados por el usuario
+		
+		for(int i = 0; i < array.length; i++) {
+			
+			System.out.println("Ingrese un numero para la posicion " + (i+1) + " del array: ");
+			
+			array[i] = entrada.nextInt();
+			
+			SumaDeArray += array[i];
+			
+		}
+		
+		//se muestran los valores de cada posicion 
+		
+		System.out.println("Los valores de cada posicion son: ");
+		
+		for(int i = 0; i < array.length; i++) {
+			
+			System.out.println(array[i] + " ");
+			
+			
+			
+			}
+		  
+		// se muestra la suma los numeros ingresados por el usuario 
+		
+		System.out.println("La suma de los numeros que ingreso es: " + SumaDeArray);
 
 	}
 
